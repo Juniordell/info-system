a4338a2210734b1797d58aa9f6f5ba9fe65363d6
Nelson Jr <juniordell123@gmail.com>
2020-09-04 10:42:36 -0300

    add Total disk memory and Free disk memory

--- calc/memory.js
+++ calc/memory.js
@@ -1,10 +1,51 @@
 const os = require("os");
 const bytes = require("bytes");
+const sys = require("systeminformation");
 
-const totalMem = bytes(os.totalmem());
-const freeMem = bytes(os.freemem());
+const totalRAMMem = bytes(os.totalmem());
+const freeRAMMem = bytes(os.freemem());
+
+async function getTotalDiskMemory() {
+  const disks = await sys.fsSize();
+  let total = 0;
+
+  for (disk of disks) {
+    if (disk.size) {
+      total += disk.size;
+    }
+  }
+
+  return bytes(total);
+}
+
+let totalDiskMem = getTotalDiskMemory()
+  .then((result) => (totalDiskMem = result))
+  .catch((err) => console.log(err));
+
+async function getFreeDiskMemory() {
+  const disks = await sys.fsSize();
+  let totalUsed = 0;
+  let total = 0;
+
+  for (disk of disks) {
+    if (disk.used) {
+      totalUsed += disk.used;
+    }
+    if (disk.size) {
+      total += disk.size;
+    }
+  }
+
+  return bytes(total - totalUsed);
+}
+
+let freeDiskMem = getFreeDiskMemory()
+  .then((result) => (freeDiskMem = result))
+  .catch((err) => console.log(err));
 
 module.exports = {
-  totalMem,
-  freeMem,
+  totalRAMMem,
+  freeRAMMem,
+  totalDiskMem,
+  freeDiskMem,
 };
